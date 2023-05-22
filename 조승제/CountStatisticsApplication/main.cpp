0e45bab0df22d573860c40ebc06d1bd903582f16
Seung-je Cho <csz0209@gmail.com>
2023-05-23 06:09:48 +0900

    feat: Application, JobPosting 클래스 임시 추가

---
+++ 조승제/CountStatisticsApplication/main.cpp
@@ -0,0 +1,81 @@
+// 헤더 선언
+#include <stdio.h>
+#include <string.h>
+
+// 상수 선언
+#define MAX_STRING 32
+#define INPUT_FILE_NAME "input.txt"
+#define OUTPUT_FILE_NAME "output.txt"
+
+// 함수 선언
+void doTask();
+void displayStatistics();
+void program_exit();
+
+// 변수 선언
+FILE* in_fp, * out_fp;
+
+int main()
+{	// 파일 입출력을 위한 초기화
+	FILE* in_fp = fopen(INPUT_FILE_NAME, "r+");
+	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
+
+	doTask();
+
+	return 0;
+}
+
+void doTask()
+{
+	// 메뉴 파싱을 위한 level 구분을 위한 변수
+	int menu_level_1 = 0, menu_level_2 = 0;
+	int is_program_exit = 0;
+
+	while (!is_program_exit)
+	{
+		// 입력파일에서 메뉴 숫자 2개를 읽기
+		fscanf(in_fp, "%d %d", &menu_level_1, &menu_level_2);
+
+		// 메뉴 구분 및 해당 연산 수행
+		switch (menu_level_1)
+		{
+		case 5:
+		{
+			switch (menu_level_2)
+			{
+			case 1: // "5.1 지원 정보 통계" 메뉴 부분
+			{
+				// displayStatistics() 함수에서 해당 기능 수행
+				displayStatistics();
+				break;
+			}
+			case 2:
+			{
+				break;
+			}
+
+			}
+
+		case 6:
+		{
+			switch (menu_level_2)
+			{
+			case 1: // "6.1. 종료" 메뉴 부분
+			{
+				program_exit();
+				is_program_exit = 1;
+				break;
+			}
+
+			}
+
+		}
+		}
+		}
+		return;
+	}
+}
+void displayStatistics()
+{
+
+}
